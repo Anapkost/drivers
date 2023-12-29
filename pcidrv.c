beef56d4b61c6374326f69f000b103bce69c7f34
Anastasia <86796106+Anapkost@users.noreply.github.com>
2023-12-29 16:14:50 +0300

    Update pcidrv.c

--- pcidrv.c
+++ pcidrv.c
@@ -1,179 +1,133 @@
-// vendor id - 8086
-// device -d - 100f
-// MAC адрес: 00:0C:29:B0:39:A8
-
-#include <linux/module.h>
 #include <linux/init.h>
+#include <linux/module.h>
 #include <linux/pci.h>
-#include <linux/fs.h>
-#include <linux/cdev.h>
-#include <linux/io.h>
-#include <linux/uaccess.h>
+#include <linux/fs.h>       
+#include <linux/if.h> 
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <linux/cdev.h> 
+#include <linux/netdevice.h>  
+#include <linux/pci.h>       
 
-#define DEVICE_NAME "pci_mac"
-#define MAC_ADDR_LENGTH 6
-
+#define MY_DRIVER "my_pci_driver"
 #define DRIVER_NAME "r8169"
-#define MAX_USER_INPUT_SIZE 10
+#define MAX_USER_INPUT_SIZE 1400
 
-static int major_number;
-static struct cdev r8169;
-static char value_from_user[MAX_USER_INPUT_SIZE];
-
+static int major_number = 510;
 static dev_t dev_num;
-static struct cdev *char_device = NULL;
-static struct pci_dev *pci_device = NULL;
-
-// Открытие файла устройства
-static int pci_mac_open(struct inode *inode, struct file *file) {
-    return 0;
-}
-
-// Закрытие файла устройства
-static int pci_mac_release(struct inode *inode, struct file *file) {
-    return 0;
-}
-
-
-static int prob(struct pci_dev *pdev, const struct pci_device_id *ent) {
-    int result;
-    dev_t dev_num;
-    
-    // Регистрируем символьный драйвер
-    result = alloc_chrdev_region(&dev_num, 0, 1, DRIVER_NAME);
-    if (result < 0) {
-        printk(KERN_ERR "Failed to allocate chrdev region\n");
-        return result;
-    }
-    
-    // Создаем устройство символьного драйвера
-    cdev_init(&r8169, &r8169_fops);
-    r8169.owner = THIS_MODULE;
-    
-    // Добавляем устройство символьного драйвера в систему
-    result = cdev_add(&r8169, dev_num, 1);
-    if (result < 0) {
-        printk(KERN_ERR "Failed to add cdev\n");
-        unregister_chrdev_region(dev_num, 1);
-        return result;
-    }
-    
-    // Устанавливаем значение major-номера
-    major_number = MAJOR(dev_num);
-    
-    printk(KERN_INFO "Registered a major number %d\n", major_number);
-    
-    return 0;
-}
-
-
-// функция для обработки IOCTL вызовов
-static long pci_mac_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
-    unsigned char mac_addr[MAC_ADDR_LENGTH];
-
-    if (cmd != 0) {
-        return -EINVAL;
-    }
-
-    if (!pci_device) {
-        printk(KERN_ERR "PCI device not found\n");
-        return -ENODEV;
-    }
-
-    // Получение MAC-адреса из PCI устройства
-
-    // Получение регистра BAR (Base Address Register)
-    int bar = pci_select_bars(pci_device, IORESOURCE_MEM);
-    if (bar == 0) {
-        printk(KERN_ERR "Failed to select BAR\n");
-        return -EFAULT;
-    }
-
-    // Получение базового адреса BAR
-    resource_size_t bar_start = pci_resource_start(pci_device, bar);
-    if (!bar_start) {
-        printk(KERN_ERR "Failed to get BAR start address\n");
-        return -EFAULT;
-    }
-
-    unsigned int *mac_ptr = ioremap(bar_start, MAC_ADDR_LENGTH);
-    if (!mac_ptr) {
-        printk(KERN_ERR "Failed to map BAR memory\n");
-        return -EFAULT;
-    }
-
-    // Копирование MAC-адреса из памяти устройства
-    memcpy(mac_addr, mac_ptr, MAC_ADDR_LENGTH);
-    iounmap(mac_ptr);
-
-    // Копирование MAC-адреса в буфер пользователя
-    if (copy_to_user((unsigned char *)arg, mac_addr, MAC_ADDR_LENGTH) != 0) {
-        return -EFAULT;
-    }
-
-    return 0;
-}
-
-// Структура операций с файлами для устройства
-static struct file_operations fops = {
-    .open = pci_mac_open,
-    .release = pci_mac_release,
-    .unlocked_ioctl = pci_mac_ioctl,
+static struct cdev cdev;
+static struct pci_device_id my_driver_id_table[] = {
+	{ PCI_DEVICE(0x10ec, 0x8168) },
+	{0,}
 };
 
-// Инициализация модуля
-static int __init pci_mac_init(void) {
-    // Регистрация номера устройства
-    int ret = alloc_chrdev_region(&dev_num, 0, 1, DEVICE_NAME);
-    if (ret < 0) {
-        printk(KERN_ERR "Failed to allocate character device\n");
-        return ret;
-    }
+MODULE_DEVICE_TABLE(pci, my_driver_id_table);
 
-    // Выделение памяти для структуры устройства
-    char_device = cdev_alloc();
-    if (!char_device) {
-        printk(KERN_ERR "Failed to allocate cdev\n");
-        unregister_chrdev_region(dev_num, 1);
-        return -ENOMEM;
-    }
+static int my_driver_probe(struct pci_dev *pdev, const struct pci_device_id *ent);
+static void my_driver_remove(struct pci_dev *pdev);
 
-    // Инициализация структуры устройства
-    cdev_init(char_device, &fops);
-    ret = cdev_add(char_device, dev_num, 1);
-    if (ret < 0) {
-        printk(KERN_ERR "Failed to add character device\n");
-        cdev_del(char_device);
-        unregister_chrdev_region(dev_num, 1);
-        return ret;
-    }
+static struct pci_driver my_driver = {
+	.name = MY_DRIVER,
+	.id_table = my_driver_id_table,
+	.probe = my_driver_probe,
+	.remove = my_driver_remove
+};
 
-    // Поиск PCI устройства по Vendor и Device ID
-    pci_device = pci_get_device(0x8086, 0x100f, NULL);
-    if (!pci_device) {
-        printk(KERN_ERR "PCI device not found\n");
-        cdev_del(char_device);
-        unregister_chrdev_region(dev_num, 1);
-        return -ENODEV;
-    }
+struct my_driver_priv {
+	u8 __iomem *hwmem;
+};
 
-    printk(KERN_INFO "PCI MAC driver initialized\n");
+
+static int __init mypci_driver_init(void)
+{
+	
+	if (cdev_add(&cdev, dev_num, 1) < 0) {
+        pr_err("Failed to add character device\n");
+        return -1;
+        }
+    return 0;    
+    }
+    
+
+static void __exit mypci_driver_exit(void)
+{
+	pci_unregister_driver(&my_driver);
+}
+
+void release_device(struct pci_dev *pdev)
+{
+	pci_release_region(pdev, pci_select_bars(pdev, IORESOURCE_MEM));
+	pci_disable_device(pdev);
+}
+
+static char value_from_user[MAX_USER_INPUT_SIZE];
+
+static int r8169_open(struct inode* inode, struct file* file) { 
+    return 0;
+}
+	
+	long my_ioctl(struct file *filep, unsigned int cmd, unsigned long arg) {
+	switch (cmd) {
+	case 0:
+	memset(value_from_user, 0, sizeof(value_from_user));
+		if (copy_from_user(value_from_user, (char *)arg, MAX_USER_INPUT_SIZE - 1) != 0) {
+		return -EFAULT;
+		};
+	printk(KERN_INFO "Значение от пользователя: %s\n", value_from_user);
+	break;
+	default:
+	return -ENOTTY;
+	}
+return 0;
+}
+
+	
+	static struct file_operations r8169_fops = {
+    .owner = THIS_MODULE,
+    .open = r8169_open,
+    .unlocked_ioctl = my_ioctl
+};
+
+static void __exit r8169_exit (void) {
+	unregister_chrdev(major_number, DRIVER_NAME);
+	printk(KERN_ALERT "Сleanup_module OK \n");
+}
+
+static int my_driver_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
+{
+	int bar, err;
+	u16 vendor, device;
+	unsigned long mmio_start,mmio_len;
+	struct my_driver_priv *drv_priv;
+	
+	if (register_chrdev(510, "DRIVER_NAME", &r8169_fops) < 0) {
+        printk(KERN_ALERT "Failed to register character driver\n");
+        }
     return 0;
 }
 
-// Выход из модуля
-static void __exit pci_mac_exit(void) {
-    if (pci_device) {
-        pci_dev_put(pci_device);
-    }
+static void my_driver_remove(struct pci_dev *pdev)
+{
+	struct my_driver_priv *drv_priv = pci_get_drvdata(pdev);
+    unregister_chrdev(0, "my_pci_device");
+    cdev_del(&cdev);
+        unregister_chrdev_region(dev_num, 1);  
 
-    cdev_del(char_device);
-    unregister_chrdev_region(dev_num, 1);
-    printk(KERN_INFO "PCI MAC driver exited\n");
+	if (drv_priv) {
+		if (drv_priv->hwmem) {
+			iounmap(drv_priv->hwmem);
+		}
+
+		kfree(drv_priv);
+	}
+
+	release_device(pdev);
 }
 
-module_init(pci_mac_init);
-module_exit(pci_mac_exit);
-
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Anastasia");
+MODULE_DESCRIPTION("Test PCI driver");
+MODULE_VERSION("0.1");
 
+module_init(mypci_driver_init);
+module_exit(mypci_driver_exit);
